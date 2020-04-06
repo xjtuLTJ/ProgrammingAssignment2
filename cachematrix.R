fb0c95c4d72151a406f1cc27d82f61eb7f298e13
xjtuLTJ <43645774+xjtuLTJ@users.noreply.github.com>
2020-04-06 20:52:18 +0800

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse;
+## cacheSolve: This function computes the inverse of the special "matrix" returned by makeCacheMatrix above, 
+## If the inverse has already been calculated (and the matrix has not changed), then cacheSolve should retrieve the inverse from the cache.
+## 
 
-## Write a short comment describing this function
+## The first function, makeCacheMatrix, creates a special "matrix", which is really a list containing a function to
+
+## 1. set the value of the matrix
+## 2. get the value of the matrix
+## 3. set the inverse of the matrix
+## 4. get the inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        s <- NULL
+        set <- function(y) {
+                x <<- y
+                s <<- NULL
+        }
+        get <- function() x
+        setsolve <- function(solve) s <<- solve
+        getsolve <- function() s
+        list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## The following function calculates inverse of the special "matrix" created with the above function. 
+## However, it first checks to see if inverse of the matrix has already been calculated. 
+## If so, it gets the inverse from the cache and skips the computation. Otherwise, 
+## it calculates the inverse in the cache via the setsolve function.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        s <- x$getsolve()
+        if(!is.null(s)) {
+                message("getting cached data")
+                return(s)
+        }
+        data <- x$get()
+        s <- solve(data, ...)
+        x$setsolve(s)
+        s
 }
